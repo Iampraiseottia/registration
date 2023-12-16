e2e7772f9455c3260e56af1cc3ea7688194892ab
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2023-12-15 19:09:37 -1200

    UI adjusted and other fields added

---
+++ registration.js
@@ -0,0 +1,5 @@
+const name = document.getElementById('name');
+const UserName = document.getElementById('Username');
+const email = document.getElementById('Email');
+const password = document.getElementById('Password');
+
